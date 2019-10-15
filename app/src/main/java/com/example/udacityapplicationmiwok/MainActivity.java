70102a716a1769b535da81e47c42b3724b174e52
Mahmoud <wisermahmoud@gmail.com>
2019-10-15 05:51:31 +0200

    done

---
+++ app/src/main/java/com/example/udacityapplicationmiwok/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.udacityapplicationmiwok;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void openNumbers_Activity(View view) {
+        startActivity(new Intent(this ,NumbersActivity.class));
+    }
+
+    public void openFamily_Activity(View view) {
+        startActivity(new Intent(this, FamilyActivity.class));
+    }
+
+    public void openPhrases_Activity (View view){
+        startActivity(new Intent(this, PhrasesActivity.class));
+    }
+
+    public void openColors_Activity (View view){
+        startActivity(new Intent(this, ColorsActivity.class));
+    }
+
+}
---
+++ app/src/main/java/com/example/udacityapplicationmiwok/WordAdapter.java
@@ -0,0 +1,44 @@
+package com.example.udacityapplicationmiwok;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class WordAdapter<W> extends ArrayAdapter {
+
+
+    public WordAdapter(Context context, ArrayList<W> words) {
+        super(context, 0 ,words);
+
+    }
+
+
+    @Override
+    public View getView(int position,  View convertView,  ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+
+        Word wordObj = (Word) getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView textView_eng = convertView.findViewById(R.id.default_text_view);
+        // set this text on the name TextView
+        textView_eng.setText(wordObj.getDefaultTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView textView_miwok = convertView.findViewById(R.id.miwok_view);
+        // set this text on the number TextView
+        textView_miwok.setText(wordObj.getMiwokTranslation());
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return convertView;    }
+}
